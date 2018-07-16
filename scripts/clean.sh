ac6258ccc6af2bee2e8f516940637e9026c03599
Joe Alden <me@joealden.com>
2018-07-16 11:21:37 +0100

    Move API out into a seperate repo

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,15 +1,11 @@
 # If node_modules dir exists, delete it.
-if test -d ./$1/node_modules
+if test -d node_modules
 then
-  rm -r ./$1/node_modules
+  rm -r node_modules
 fi
 
-# If first arg to script is 'client':
-if [ $1 == "client" ]
+# If .next dir exists, delete it.
+if test -d .next
 then
-  # If .next dir exists, delete it.
-  if test -d ./$1/.next
-  then
-    rm -r ./$1/.next
-  fi
+  rm -r .next
 fi
