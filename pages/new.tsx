3416c32f910f9d8697a84c01d25aec40a3dddd39
Joe Alden <me@joealden.com>
2018-07-21 12:16:53 +0100

    Remove the test query that was in the new chat page

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,31 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
 
 import Layout from "../components/layout";
 
-const userFriendQuery = gql`
-  query {
-    userFriends(userId: "test") {
-      id
-      email
-    }
-  }
-`;
-
 const NewChatPage = () => (
   <Layout mainTitle="New Chat">
-    <ChatWrapper>
-      <Query query={userFriendQuery}>
-        {({ loading, error, data }) => {
-          if (loading) return <p>Loading...</p>;
-          if (error) return <p>Error :(</p>;
-
-          return <p>{data.userFriends[0].email}</p>;
-        }}
-      </Query>
-    </ChatWrapper>
+    <ChatWrapper>New Chat Page</ChatWrapper>
   </Layout>
 );
 
